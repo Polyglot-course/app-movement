f133fba282eebb160a0de5b5efc87b0dea7a8302
Mateo Loaiza Rios <mateo@nereus.io>
2025-03-15 11:21:27 -0500

    changes in code, and pipeline implementation

--- src/app/controllers/movement.controller.js
+++ src/app/controllers/movement.controller.js
@@ -1,6 +1,8 @@
+const logProvider = require('../middlewares/logprovider')
 const movementService = require('../services/movement.service')
 
 const getMovementByAccountId = async (req, res) => {
+    logProvider.info('Get Movement By AccountId');
     const accountId = parseInt(req.params.accountId)
     return res.send(await movementService.getMovementByAccountId(accountId))
 }
--- src/app/services/movement.service.js
+++ src/app/services/movement.service.js
@@ -1,9 +1,37 @@
+const redis = require('redis')
 const movementRepository = require('../repositories/movement.repository')
-
+ 
+const client = redis.createClient({
+    url: process.env.DB_REDIS_URL
+})
+client.connect();
+client.on('error', function (err) {
+    console.log('Error ' + err)
+})
+ 
+ 
 const movementService = {
     getMovementByAccountId: async (accountId) => {
-        return await movementRepository.getMovementByAccountId(accountId)
+        const key = `key-movement-${accountId}`
+        const reply = await client.get(key)
+        if (reply === null) {
+            console.log('Register information in redis')
+            var result = await movementRepository.getMovementByAccountId(accountId)
+            await client.set(key, JSON.stringify(result), 'EX', 30)
+            return result
+        }
+        else {
+            console.log('Showing redis information')
+            return JSON.parse(reply)
+        }
     }
 }
-
+ 
+ 
+// const movementService = {
+//     getMovementByAccountId: async (accountId) => {
+//         return await movementRepository.getMovementByAccountId(accountId)
+//     }
+// }
+ 
 module.exports = movementService
